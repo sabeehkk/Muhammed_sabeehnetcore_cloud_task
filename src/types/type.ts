dbf5910102fc69b2bfa5074ed57efe20b167e751
sabeehkk <sabeehkadungalloor573@gmail.com>
2024-08-20 06:57:38 +0530

    completed ui design

--- src/types/type.ts
+++ src/types/type.ts
@@ -7,36 +7,38 @@ export type optionType = {
 export type forecastType = {
     name: string
     country: string
-    list: [
-      {
-        dt: number
-        main: {
-          feels_like: number
-          humidity: number
-          pressure: number
-          temp: number
-          temp_max: number
-          temp_min: number
-        }
-        weather: [
-          {
-            main: string
-            icon: string
-            description: string
+    list: {
+      list: [
+        {
+          dt: number
+          main: {
+            feels_like: number
+            humidity: number
+            pressure: number
+            temp: number
+            temp_max: number
+            temp_min: number
           }
-        ]
-        wind: {
-          speed: number
-          gust: number
-          deg: number
+          weather: [
+            {
+              main: string
+              icon: string
+              description: string
+            }
+          ]
+          wind: {
+            speed: number
+            gust: number
+            deg: number
+          }
+          clouds: {
+            all: number
+          }
+          pop: number
+          visibility: number
         }
-        clouds: {
-          all: number
-        }
-        pop: number
-        visibility: number
-      }
-    ]
+      ]
+    }
     sunrise: number
     sunset: number
   }
